b4cc79b7b494f386420befb732886eece208dad5
ClaudePal20 <claudio.palacio@uabc.edu.mx>
2024-09-01 18:16:57 -0700

    derechos de autor agregados a todos los archivos

--- _mesa/simulacion.2/enfermedad.py
+++ _mesa/simulacion.2/enfermedad.py
@@ -1,3 +1,26 @@
+######################################################################
+## PythonSyntheticElectronicMedicalRecords 1.0
+## Modelo computacional de registros medicos electrónicos sintéticos 
+## en Python.
+######################################################################
+## This software are distributed using the Creative Commons Public
+## License "Attribution-NonCommercial-ShareAlike 4.0 International"
+## https://creativecommons.org/licenses/by-nc-sa/4.0/
+######################################################################
+## Author: Manuel Castañón Puga, Claudio Emiliano Palacio Martínez, 
+## Ricardo Fernando Rosales Cisneros, Carelia Guadalupe Gaxiola
+## Pacheco, Luis Enrique Palafox Maestre.
+## Copyright: Copyright 2024, Universidad Autónoma de Baja California.
+######################################################################
+## Credits: matplotlib, mesa, Flask, SQLAlchemy, Faker, requests, 
+## tqdm, pandas and click libraries.
+## License: CC BY-NC-SA 4.0
+## Version: 1.0.0
+## Mmaintainer: https://github.com/pugapuga.
+## Email: puga@uabc.edu.mx
+## Status: Released.
+######################################################################
+
 # enfermedad.py
 
 class Enfermedad:
